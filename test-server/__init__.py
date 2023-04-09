171238fa5edad51791369f23e3c654ff8bfda98a
Alexey_Protasov <aalxalx@bk.ru>
2023-04-09 20:03:34 +0500

    test-server-0.2

--- test-server/__init__.py
+++ test-server/__init__.py
@@ -32,4 +32,8 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import index
+    app.register_blueprint(index.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
---
+++ test-server/index.py
@@ -0,0 +1,17 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from .auth import login_required
+from .database import get_db
+
+bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    return render_template('index.html')
+
+@bp.route('/card')
+def card():
+    return render_template('card.html')
