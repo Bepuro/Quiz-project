ed62feff7f6867156cb0201187e2f8d4589e1a2a
Bepuro <yazov.nik.yandex.ru>
2023-04-16 23:13:18 +0500

    refact card

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ var cards = document.querySelectorAll('.card');
 let dir = {0:['мама','папа'],1:['тетя','дядя'],2:['сестра','брат']};
 let i = 0;
 
+document.getElementById("one").addEventListener(() => {
+    
+})
 document.getElementById("one").onclick = function() {
+    console.log("232")
     if (Object.keys(dir).length === i) {
+
         document.getElementById("f").innerHTML = "Я";
         document.getElementById("b").innerHTML = "Семья";
         i =0;
